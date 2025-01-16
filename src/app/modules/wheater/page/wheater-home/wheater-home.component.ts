d47064bfa3ccf603ac55acdd3a15dbb35a92c23e
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2025-01-16 13:13:42 -0300

    Implementando formulário de pesquisa

--- src/app/modules/wheater/page/wheater-home/wheater-home.component.ts
+++ src/app/modules/wheater/page/wheater-home/wheater-home.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WheaterService } from '../../services/wheater.service';
 import { IWheaterDatas } from 'src/app/models/interfaces/IWheaterDatas';
+import { Subject, takeUntil } from 'rxjs';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-wheater-home',
   templateUrl: './wheater-home.component.html',
   styleUrls: [],
 })
-export class WheaterHomeComponent implements OnInit {
-  private initialCity = 'Salvador';
+export class WheaterHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+
+  initialCity = 'Salvador';
   wheaterDatas!: IWheaterDatas;
+  searchIcon = faMagnifyingGlass
 
   constructor(private weatherService: WheaterService) {}
 
@@ -18,13 +23,26 @@ export class WheaterHomeComponent implements OnInit {
   }
 
   getWeatherDatas(city: string): void {
-    this.weatherService.getWeatherDatas(city).subscribe({
-      next: (response) => {
-        response && (this.wheaterDatas = response);
-        console.log(this.wheaterDatas);
-      },
+    this.weatherService
+      .getWeatherDatas(city)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          response && (this.wheaterDatas = response);
+          console.log(this.wheaterDatas);
+        },
 
-      error: (e) => console.log(e),
-    });
+        error: (e) => console.log(e),
+      });
+  }
+
+  onSubmit(): void {
+    this.getWeatherDatas(this.initialCity);
+    this.initialCity = '';
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
